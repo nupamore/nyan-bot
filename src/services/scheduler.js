8dd95a5dc01629b08b821864b823974e3a03061a
nupamore <nupamore@gmail.com>
2020-12-16 14:28:36 +0900

    fix: cron

--- src/services/scheduler.js
+++ src/services/scheduler.js
@@ -2,19 +2,19 @@ const config = require('../../config')
 const schedule = require('node-schedule')
 const { getInfo } = require('./info')
 const { addHistory } = require('./db')
-const util = require('../services/util')
+const util = require('./util')
 
 async function saveInfo(client) {
     const channel = await client.channels.fetch(config.channelId)
-    schedule.scheduleJob('*/10 * * * * *', async () => {
+    schedule.scheduleJob('0 0 0 * * *', async () => {
         const info = await getInfo()
         info.date = info.date.toLocaleString()
         info.diff = 0
         addHistory(info)
 
         const msg = `**${info.date}**
-총 자산: ${util.money(info.won)} (자산의 ${info.coinPerWon})
-코인 매수가: ${util.money(info.coin)}`
+총 자산: ${util.money(info.won)}
+코인 매수가: ${util.money(info.coin)} (자산의 ${info.coinPerWon})`
         channel.send(msg)
     })
 }
